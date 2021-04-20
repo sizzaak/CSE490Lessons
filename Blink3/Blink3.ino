8701ce3319a37f974987ea2ad54bf54af223b640
sizzaak <sizzaak@cs.washington.edu>
2021-04-19 18:50:00 -0700

    Added serial output to blink program

--- Blink3/Blink3.ino
+++ Blink3/Blink3.ino
@@ -2,12 +2,16 @@ const int LED_OUTPUT_PIN = 3;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_OUTPUT_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_OUTPUT_PIN, HIGH);
+  Serial.println("The LED is on");
   delay(1000);
+  
   digitalWrite(LED_OUTPUT_PIN, LOW);
+  Serial.println("The LED is off");
   delay(1000);
 }
